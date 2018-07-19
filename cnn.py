2db06c04ddc8bcdfa866d2b6cfe0ae37a66cdf71
KingHypnos <41354072+KingHypnos@users.noreply.github.com>
2018-07-19 08:25:11 -0500

    Changed Comments
    Moving pre-requisites to a more appropriate place, and simplified comments

--- cnn.py
+++ cnn.py
@@ -1,47 +1,30 @@
-# Convolutional Neural Network
-
-# Installing Theano
-# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
-
-# Installing Tensorflow
-# pip install tensorflow
-
-# Installing Keras
-# pip install --upgrade keras
-
-# Part 1 - Building the CNN
-
-# Importing the Keras libraries and packages
 from keras.models import Sequential
 from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
 
-# Initialising the CNN
 classifier = Sequential()
 
-# Step 1 - Convolution
 classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
 
-# Step 2 - Pooling
+#convolution layer 1
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
 
-# Adding a second convolutional layer
+#convolution layer 2
 classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
 
-# Step 3 - Flattening
 classifier.add(Flatten())
 
-# Step 4 - Full connection
+#dense layers
 classifier.add(Dense(units = 128, activation = 'relu'))
 classifier.add(Dense(units = 1, activation = 'sigmoid'))
 
-# Compiling the CNN
+#compile
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
-# Part 2 - Fitting the CNN to the images
+# apply CNN to images
 
 from keras.preprocessing.image import ImageDataGenerator
 
