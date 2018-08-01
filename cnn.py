f88624c2b23d89e9078a5be3625625ddd36f59cb
KingHypnos <41354072+KingHypnos@users.noreply.github.com>
2018-07-31 21:42:53 -0500

    Added multiclass classifier, and model saving capabilities

--- cnn.py
+++ cnn.py
@@ -1,4 +1,7 @@
+import json
 from keras.models import Sequential
+from keras.models import model_from_json
+from keras.models import load_model
 from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
@@ -6,9 +9,8 @@ from keras.layers import Dense
 
 classifier = Sequential()
 
-classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
-
 #convolution layer 1
+classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
 
 #convolution layer 2
@@ -19,10 +21,10 @@ classifier.add(Flatten())
 
 #dense layers
 classifier.add(Dense(units = 128, activation = 'relu'))
-classifier.add(Dense(units = 1, activation = 'sigmoid'))
+classifier.add(Dense(units = 11, activation = 'sigmoid'))
 
 #compile
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+classifier.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
 
 # apply CNN to images
 
@@ -38,15 +40,22 @@ test_datagen = ImageDataGenerator(rescale = 1./255)
 training_set = train_datagen.flow_from_directory('dataset/training_set',
                                                  target_size = (64, 64),
                                                  batch_size = 32,
-                                                 class_mode = 'binary')
+                                                 class_mode = 'categorical')
 
 test_set = test_datagen.flow_from_directory('dataset/test_set',
                                             target_size = (64, 64),
                                             batch_size = 32,
-                                            class_mode = 'binary')
+                                            class_mode = 'categorical')
 
 classifier.fit_generator(training_set,
-                         steps_per_epoch = 8000,
+                         steps_per_epoch = 397,
                          epochs = 25,
                          validation_data = test_set,
-                         validation_steps = 2000)
+                         validation_steps = 397)
+						 
+# Save the weights
+classifier.save_weights('model_weights.h5')
+
+# Save the model architecture
+with open('model_architecture.json', 'w') as f:
+    f.write(classifier.to_json())
